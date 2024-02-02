fc40d825dcac640d3f8644d4646d8ca7d3af50e3
ronak <rmal0008@synchronoss.com>
2024-02-03 01:27:00 +0530

    Added error handling for n/w layer

---
+++ Bt-Demo/NetworkService/UserError.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkError.swift
+//  Bt-Demo
+//
+//  Created by Ronak Malick on 03/02/24.
+//
+
+import Foundation
+
+enum UserError: Error {
+    case urlNotValid
+    case parsingFailed
+    case noData
+    case maxPageCountReached
+}
--- Bt-Demo/NetworkService/UserNetworkTask.swift
+++ Bt-Demo/NetworkService/UserNetworkTask.swift
@@ -7,23 +7,17 @@
 
 import Foundation
 
-class NetworkService {
+class UserNetworkTask {
     func fetch(url: URL, onCompletion: @escaping (Users) -> (Void)) {
-        
         let req = URLRequest(url: url)
-        
         let session = URLSession(configuration: .default)
-        
         session.dataTask(with: req) { data, resp, err in
-            
             guard let unwrappedData = data else { return }
-            
             do {
                 let decoder = JSONDecoder()
                 let unwrapedModel = try decoder.decode(Users.self, from: unwrappedData)
                 print(unwrapedModel)
                 onCompletion(unwrapedModel)
-                
             } catch {
                 print("parser failure \(error)")
             }
